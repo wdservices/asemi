7cc43773368a79b735d7ef68455783a30483566c
Waves Digital Services <hello.wdservices@gmail.com>
2025-07-14 21:57:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using Environment Variables
 const firebaseConfig = {
-    apiKey: "AIzaSyCdFu24SWTYh9TrH1pYaU4g6cKcx11XRRE",
-    authDomain: "asemi-authentication.firebaseapp.com",
-    projectId: "asemi-authentication",
-    storageBucket: "asemi-authentication.appspot.com",
-    messagingSenderId: "226350315554",
-    appId: "1:226350315554:web:5f3174a99e4d49dec9f27b"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 
