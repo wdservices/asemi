f657a1c42f90730ccc1dd47bbf074eaf753caa4b
Waves Digital Services <hello.wdservices@gmail.com>
2025-07-15 00:01:39 +0000

    or should i provide another api key?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration using Environment Variables
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    apiKey: "AIzaSyCWk88OprZ1pe5QYJbkTgQY0Ao6n-uP1xw",
+    authDomain: "asemi-authentication.firebaseapp.com",
+    projectId: "asemi-authentication",
+    storageBucket: "asemi-authentication.appspot.com",
+    messagingSenderId: "1089232834079",
+    appId: "1:1089232834079:web:ce2db2312c614f1b419e35"
 };
 
 
