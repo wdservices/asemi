d069acc5c3d52e155c254206a783399b8846f97a
Waves Digital Services <hello.wdservices@gmail.com>
2025-07-29 00:18:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.rbl.ms',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
